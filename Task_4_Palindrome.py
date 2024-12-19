874e4808d41129c655abd1972c29ba57d8c0180a
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2024-12-19 15:35:24 +0000

    Task-4 Palindrome completed

--- Task_4_Palindrome.py
+++ Task_4_Palindrome.py
@@ -12,49 +12,54 @@
 
 def isPalindrome(word):
     
-    # IF PASSED ARGUMENT IS LENGTH 1 
+    # IF PASSED ARGUMENT IS LENGTH <=1 
         # RETURN TRUE
-    if len(word) == 1:
+    if len(word) <= 1:
         return True
     
     # IF FIRST AND LAST CHARACTERS ARE NOT EQUAL
         # USE STRING SLICING TO ACCESS LETTERS
         # RETURN FALSE
-    first = word[0]
-    last  = word[-1]
-    print(first)
-    print(last)
+    # first = word[0]
+    # last  = word[-1]
+    # print(first)
+    # print(last)
+    if (word[0] != word[-1]):
+        return False
         
     # DO A RECURSIVE CALL 
+        # WE KNOW THAT THE FIRST AND LAST CHARACTERS OF THE WORD ARE THE SAME
         # PASS THE WORD TO THE FUNCTION WITHOUT THE FIRST AND LAST CHARACTERS
-    return False
-
-
-# Get input from user
-attempt = 0
-max_attempt = 5
-
-palindrome = False
-
-while palindrome == False:
-
-    word_to_check = input("Enter a word to see if it is a palindrome. You have 5 attempts. :: ")
-
-    attempt += 1
+    # print (f"{word[1:-1]}")
     
-    if (len(word_to_check)) == 0:
-        print ("The word can't be an empty string. 1 attempt used.  Try again.")
+    return isPalindrome(word[1:-1])
+
+
+if __name__=="__main__":
+    
+    # Get input from user
+    attempt = 0
+    max_attempt = 3
+
+    palindrome = False
+
+    while palindrome == False:
+
+        word_to_check = input(f"Enter a word to see if it is a palindrome. You have {max_attempt - attempt} attempts. :: ")
+
+        attempt += 1
+        
+        if (len(word_to_check)) == 0:
+            print ("The word can't be an empty string. 1 attempt used.  Try again.")
+        else:
+            
+            palindrome = isPalindrome(word_to_check.upper())
+            
+            if attempt == max_attempt and palindrome == False:
+                print (f"Reached maximim attempt of {max_attempt}. Exiting")
+                break
+
+    if palindrome == False:
+        print (f"No palindrome were enetered after {max_attempt} attempts.")
     else:
-        
-        palindrome = isPalindrome(word_to_check)
-        
-        if attempt == max_attempt and palindrome == False:
-            print (f"Reached maximim attempt of {max_attempt}. Exiting")
-            break
-
-if palindrome == False:
-    print (f"No palindrome were enetered after {max_attempt} attempts.")
-else:
-    print (f"'{word_to_check}' ia a palindrome")
-
-
+        print (f"'{word_to_check}' ia a palindrome.")
