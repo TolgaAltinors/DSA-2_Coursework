b67da20e172fd0c2913d2ae088997cf5e719b119
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2024-12-19 14:45:17 +0000

    Added control logic for user interaction.

---
+++ Task_4_Palindrome.py
@@ -0,0 +1,54 @@
+"""A function that checks whether a word is a palindrome or not
+
+    Arguments:
+        word {String} -- Word to be checked
+        
+    Raises:
+        AssertionError: _description_
+
+    Returns:
+        Bool -- Whether passed string is a palindrome or not
+"""
+
+def isPalindrome(word):
+    
+
+    # IF PASSED ARGUMENT IS LENGTH 1 
+        # RETURN TRUE
+    
+    # IF FIRST AND LAST CHARACTERS ARE NOT EQUAL
+        # USE STRING SLICING TO ACCESS LETTERS
+        # RETURN FALSE
+        
+    # DO A RECURSIVE CALL 
+        # PASS THE WORD TO THE FUNCTION WITHOUT THE FIRST AND LAST CHARACTERS
+    return False
+
+
+# Get input from user
+attempt = 0
+max_attempt = 5
+
+palindrome = False
+
+while palindrome == False:
+
+    word_to_check = input("Enter a word to check if it is a palindrome. You have 5 attempts. :: ")
+
+    if (len(word_to_check)) == 0:
+        print ("The word can't be an empty string. Try again.")
+    else:
+        attempt += 1
+        
+        palindrome = isPalindrome(word_to_check)
+        
+        if attempt == max_attempt and palindrome == False:
+            print (f"Reached maximim attempt of {max_attempt}. Exiting")
+            break
+
+if palindrome == False:
+    print (f"No palindrome were enetered after {max_attempt} attempts.")
+else:
+    print (f"'{word_to_check}' ia a palindrome")
+
+
