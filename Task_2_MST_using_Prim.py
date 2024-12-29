814b702a6a3c62d2344b0295d22b3cc3e990281f
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2024-12-29 11:44:43 +0000

    hard code node positions and add title to graph

--- Task_2_MST_using_Prim.py
+++ Task_2_MST_using_Prim.py
@@ -1,68 +1,55 @@
+import heapq
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
 def add_node_positions():
-    start_x = 0.05
-    start_y = 0.05
 
-    x_adj = 0.0013
-    y_adj = 0.0011
-
-    node_pos = {"A" : [start_x, start_y]}
-
-    next_x = start_x + x_adj
-    next_y = start_y + y_adj
-    node_pos["B"] = [next_x, next_y]
-
-    next_x = start_x + x_adj
-    next_y = start_y - y_adj
-    node_pos["G"] = [next_x, next_y]
-
-    x_adj = 0.0030
-    next_x = next_x + x_adj
-    next_y = start_y + y_adj
-    node_pos["D"] = [next_x, next_y]
-
-    next_y = start_y - y_adj
-    node_pos["E"] = [next_x, next_y]
-
-    next_y = start_y
-    node_pos["C"] = [next_x, next_y]
-
-    x_adj = 0.0025
-    next_x = next_x + x_adj
-    next_y = start_y + y_adj
-    node_pos["F"] = [next_x, next_y]
-
-    next_y = start_y - y_adj
-    node_pos["Z"] = [next_x, next_y]
+    node_pos = {
+        "A" : [ 0, 2],
+        "B" : [ 1, 3],
+        "G" : [ 1, 1],
+        "D" : [ 2, 3],
+        "E" : [ 2, 1],
+        "C" : [ 2, 2],
+        "F" : [ 3, 3],
+        "Z" : [ 3, 1]      
+    }
 
     return node_pos    
 
-
-def display_graph(edges, node_pos):
+def create_original_graph(G, edges, node_pos):
     
-    G = nx.Graph()
-
     G.add_weighted_edges_from(edges)
 
     pos = nx.circular_layout(G)
 
-    # overwrite node x&y positions
-    for k, v in pos.items():
-        pos[k] = node_pos[k]
-
-    nx.draw(G, pos, with_labels=True, node_size=700, node_color='lightblue', font_size=12, font_weight='bold')
+    nx.draw(G,
+            pos=node_pos,
+            with_labels=True,
+            node_size=700,
+            node_color='lightblue',
+            font_size=12,
+            font_weight='bold'
+            )
 
     edge_labels = nx.get_edge_attributes(G, 'weight')
 
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-
-    plt.show()
+    nx.draw_networkx_edge_labels(G, pos=node_pos, edge_labels=edge_labels)
     
+    plt.suptitle("Original graph",color='red')
+    plt.margins(0.2, 0.2)
+    plt.show()
 
 
 if __name__ == '__main__':
+
+
+    fig = plt.figure(figsize=(5, 4), frameon=False)
+    
+    # Create a graph objects
+    G = nx.Graph()
+    G_mst = nx.Graph()
 
     nodes = ["A", "B", "G", "D", "E", "C", "F", "Z"]
 
@@ -83,4 +70,12 @@ if __name__ == '__main__':
     node_pos = add_node_positions()
 
     # display original graph
-    display_graph(edges, node_pos)
+    create_original_graph(G, edges, node_pos)
+
+    # create adjancey list to hold each nodes' neigbours
+    
+    
+    # find mst (G, adj list, start node)
+    
+    
+    # show final mst
