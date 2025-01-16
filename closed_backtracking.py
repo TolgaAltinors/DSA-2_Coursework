f62a8c4b41ce139cbb265d7640fa1780002a7f7a
TolgaAltinors <50206787+TolgaAltinors@users.noreply.github.com>
2025-01-16 19:07:06 +0000

    start work on closed back tracking

--- closed_backtracking.py
+++ closed_backtracking.py
@@ -6,6 +6,8 @@ from helpers import check_move_on_board
 x_moves = helpers.get_x_moves()
 y_moves = helpers.get_y_moves()
 
+x_y_moves = helpers.zip_x_y_moves()
+
 # create the chess board
 row_count = helpers.get_row_count()
 col_count = helpers.get_col_count()
@@ -21,10 +23,22 @@ print("*** Starting board ***")
 print(chess_board)
 print()
 
+# Define possible moves by current x and y
+def possible_moves(x, y):
+    knights_moves = []
+    for add_x, add_y in x_y_moves:
+        knights_moves.append((x + add_x, y + add_y))
+    return knights_moves
+
+
+def find_knights_tour(board, x, y):
+    
+    knights_moves = possible_moves(x, y)
+
+
 def closed_backtracking_knights_tour():
     
     print("Closed knight's tour using backtracking")
     print("NOT YET IMPLEMENTED")
     
-    zip_lists = helpers.zip_x_y_moves()
-    print(zip_lists)
+    find_knights_tour(chess_board, x=0, y=0)
